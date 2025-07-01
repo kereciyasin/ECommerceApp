8f0edefff866b9ac8c9cf240cb2b690f9629a282
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-01 18:54:46 +0200

    add ECommerceDbContext with DbSets for entities

---
+++ ECommerceApp.Data/Context/ECommerceDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Data.Context
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=KERECI\\SQLEXPRESS;Database=ECommerceDbContext;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+    }
+}
