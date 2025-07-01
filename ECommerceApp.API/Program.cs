c843cee636a1f7236b2fbd5dd88c55f63cf0ca8f
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-01 19:44:39 +0200

    feat: configure DbContext in API and move connection string to appsettings.json

--- ECommerceApp.API/Program.cs
+++ ECommerceApp.API/Program.cs
@@ -1,3 +1,6 @@
+using ECommerceApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerceApp.API
 {
     public class Program
@@ -7,28 +10,17 @@ namespace ECommerceApp.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<ECommerceDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllers();
 
             app.Run();
         }
--- ECommerceApp.Data/Context/ECommerceDbContext.cs
+++ ECommerceApp.Data/Context/ECommerceDbContext.cs
@@ -13,10 +13,6 @@ namespace ECommerceApp.Data.Context
         public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
         {
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=KERECI\\SQLEXPRESS;Database=ECommerceDbContext;Integrated Security=True;TrustServerCertificate=True");
-        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
