cdbfbf6f68b8f172eba9024b3b57e7eb2fac776c
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-02 10:22:06 +0200

    feat: add CategoryDto and CategoryDtoValidator for category validation and mapping

---
+++ ECommerceApp.Entities/Validator/CategoryDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Entities.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Entities.Validator
+{
+    public class CategoryDtoValidator : AbstractValidator<CategoryDto>
+    {
+        public CategoryDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters");
+        }
+    }
+}
