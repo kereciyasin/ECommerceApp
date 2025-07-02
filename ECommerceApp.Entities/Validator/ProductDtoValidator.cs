56690e5a3b92450e19b8c89d648cf4710268b4f2
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-02 10:10:17 +0200

    feat: add ProductDto and ProductDtoValidator for product validation and mapping

---
+++ ECommerceApp.Entities/Validator/ProductDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceApp.Entities.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Entities.Validator
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero");
+
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("CategoryId must be greater than zero");
+        }
+    }
+}
